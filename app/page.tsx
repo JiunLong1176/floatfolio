dc0efb8fae1be63f4332c392559d637e93587d6c
JiunLong1176 <jiun472@gmail.com>
2026-05-16 02:29:23 +0800

    handles reset password url

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,14 @@
 import { redirect } from 'next/navigation'
 
-export default function Home() {
-  redirect('/dashboard')
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<Record<string, string>>;
+}) {
+  const params = await searchParams;
+  const code = params['code'];
+  if (code) {
+    redirect(`/auth/reset-password?code=${encodeURIComponent(code)}`);
+  }
+  redirect('/dashboard');
 }
